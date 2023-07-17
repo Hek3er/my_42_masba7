221077f04e368d72a1139f5d00e03e45a35268b7
Aymen Zainabi <51007035+Hek3er@users.noreply.github.com>
2023-07-17 22:36:00 +0100

    Add files via upload

---
+++ My Days/C00/ex06/ft_print_comb2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azainabi <azainabi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 17:47:07 by azainabi          #+#    #+#             */
+/*   Updated: 2023/07/12 17:58:10 by azainabi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_prnt(int i, int j)
+{
+	ft_putchar('0' + i / 10);
+	ft_putchar('0' + i % 10);
+	ft_putchar(' ');
+	ft_putchar('0' + j / 10);
+	ft_putchar('0' + j % 10);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_prnt(i, j);
+			if (i != 98)
+			{
+				ft_putchar(' ');
+				ft_putchar(',');
+			}
+			j++;
+		}
+		i++;
+	}	
+}
