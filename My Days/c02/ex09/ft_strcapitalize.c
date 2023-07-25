5e6cdd262729d882cc6db0d3da0fc4244507a306
Aymen Zainabi <51007035+Hek3er@users.noreply.github.com>
2023-07-25 17:27:23 +0100

    Add files via upload

---
+++ My Days/c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azainabi <azainabi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/18 14:45:51 by azainabi          #+#    #+#             */
+/*   Updated: 2023/07/18 23:29:41 by azainabi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (j == 1)
+			{
+				str[i] -= 32;
+				j = 0;
+			}
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			j = 0;
+		else
+			j = 1;
+		i++;
+	}
+	return (str);
+}
